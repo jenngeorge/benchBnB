9896436df6632769359245144b1bf70638d870ee
Jennifer Georgevich <jenngeorge@berkeley.edu>
2016-08-29 19:20:09 -0700

    add React Router

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import RootReducer from './reducers/root_reducer';
-import { Provider } from 'react-redux';
 import Root from './components/root';
 
-
-import {fetchBenches} from './util/bench_api_util';
-
-import {requestBenches} from './actions/bench_actions';
-
-window.fetchBenches = fetchBenches;
-window.requestBenches = requestBenches;
-
-window.Store = configureStore();
+// testing
+// import {fetchBenches} from './util/bench_api_util';
+//
+// import {requestBenches} from './actions/bench_actions';
+//
+// window.fetchBenches = fetchBenches;
+// window.requestBenches = requestBenches;
+//
+// window.Store = configureStore();
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
-
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
---
+++ frontend/components/app.jsx
@@ -0,0 +1,6 @@
+export const App = ({children}) => (
+  <div>
+    <h1> Bench BnB </h1>
+    {children}
+  </div>
+);
